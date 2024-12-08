5a49b495e4f6c62ca5fe1a208a12a93b1069d9f8
PigeonXian <470688162@qq.com>
2024-12-09 00:25:32 +0800

    完成login前端设计

--- readme.md
+++ readme.md
@@ -1,8 +1,29 @@
-# README
-这是BUPT酒店管理系统的login/signin部分
-## 跳转
-计划完成以下部分：  
-1. 酒店前台
-2. 酒店经理
+# BUPT_hotel_login
+
+## 接口
+计划实现以下用户的login/sign in：  
+1. 酒店经理
+2. 酒店前台
 3. 酒店管理员
-4. 顾客
+4. 酒店顾客
+
+## 进度
+12/9 完成login界面的前端设计
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center; background-color: #0f9876;">
+        <div style="display: flex; background-color: white; width: 50%; border-radius: 5px; overflow: hidden;">
+            <div style="flex: 1;">
+                <img src="@/assets/login.jpg" alt="" style="width: 100%;">
+            </div>
+            <div style="flex: 1; display: flex; align-items: center; justify-content: center;">
+                <el-form :model="user" style="width: 80%">
+                    <div style="font-size: 20px; font-weight: bold; text-align: center; margin-bottom: 20px;">
+                        欢迎登录BUPT酒店管理系统
+                    </div>
+                    <el-form-item prop="username">
+                        <el-input prefix-icon="el-icon-user" size="medium" placeholder="请输入账号:3" v-model="user.username"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input prefix-icon="el-icon-lock" size="medium" show-password placeholder="请输入密码:3" v-model="user.password"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="validCode">
+                        <div style="display: flex;">
+                            <el-input prefix-icon="el-icon-circle-check" size="medium" style="flex: 1"></el-input>
+                            <div style="flex: 1">
+                                <valid-code></valid-code>
+                            </div>
+                        </div>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" style="width: 100%; background-color: #0f9876;">登录</el-button>
+                    </el-form-item>
+                    <div style="display: flex">
+                        <div style="flex: 1;">还没有账号？<span style="color: #0f9876; cursor: pointer;">注册:3</span></div>
+                        <div style="flex: 1; text-align: right;"><span style="color: #0f9876; cursor: pointer;">忘记密码</span></div>
+                    </div>
+                </el-form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ValidCode from '@/components/ValidCode.vue';
+
+export default {
+    name: "Login",
+    components: {
+        ValidCode
+    },
+    data() {
+        return{
+            user: {
+                username: '',
+                password: ''
+            }
+        }
+    },
+    created(){
+
+    }
+}
+</script>
+
